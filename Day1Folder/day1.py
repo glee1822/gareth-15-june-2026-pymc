f786821980c8c11d324ae6a92ba34e2fd34660b9
glee1822 <geelee2219@gmail.com>
2026-06-15 03:53:01 +0000

    Jun 15, 2026, 3:53 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,41 +1,107 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
+
 
 ########################################################################
 # Task 1:
-
+# print("h3110")
 
 
 ########################################################################
 # Task 2:
-
+# print("my name is gareth")
+# print("i am 13 years old")
+# print("i attend raffles institution (very tuff)")
+# print("if i had $2000 i would invest it")
 
 
 ########################################################################
 # Task 3:
-
+# animal1 =  "gryphon"
+# animal2 = "wolf"
+# animal3 = "leopard"
+# print(animal1)
+# print(animal2)
+# print(animal3)
 
 
 ########################################################################
 # Task 4:
-
+# var1 = 1
+# var2 = 1
+# print(var1 + var2)
+# print(var1 - var2)
+# print(var1 * var2)
+# print(var1 / var2)
 
 
 ########################################################################
 # Task 5:
+# name = "gareth"
+# food = "chips"
+# print("my name is " + name + ". i like " + food)
 
 
 
 ########################################################################
 # Task 6:
-
+# def Intro():
+#     print("my name is gareth")
+#     print("i am 13 years old")
+#     print("i attend raffles institution (very tuff)")
+#     print("if i had $2000 i would invest it")
+# Intro()
 
 
 ########################################################################
 # Task 7:
+# def addition(num1, num2):
+#     print(num1 + num2)
+
+# x = int(input("num1 "))
+# y = int(input("num2 "))
+# addition(x, y)
 
 
 
 ########################################################################
 # Additional exercises:
+
+# print("Hey")
+# print("This is really simple but important")
+# print("Python reads from the top to the bottom")
+# print("Bye")
+
+# name = "gareth"
+# print(name)
+
+# title = "supreme overlord"
+# name = "gareth"
+# command = "die"
+# print(title + " " + name + " commands you to " + command)
+
+# def get_out():
+#     print("get out")
+#     print("i hope never to se u again :)")
+# get_out()
+
+# x = 0.5
+# y = 0.5
+# xx = x + 2.5
+# yy = 0.5 * 2
+# print(xx + yy)
+
+# x = 5
+# y = 10
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+
+# daily_allowance = int(input("how much"))
+# month = daily_allowance * 30
+# year = daily_allowance * 365
+# p1_to_sec4 = daily_allowance * 3650
+# print(month + year + p1_to_sec4)
+
+
