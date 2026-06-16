d02a9c56b16e2a4922169bd6f640b1ab7e074a6a
glee1822 <geelee2219@gmail.com>
2026-06-16 01:32:52 +0000

    Jun 16, 2026, 1:32 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -49,14 +49,14 @@ print("hello from day3")
 
 ########################################################################
 # Task 5:
-num1 = int(input("num1"))
-num2 = int(input("num2"))
-if num1 > num2:
-    print("num1 is larger than num2")
-elif num1 < num2:
-    print("num1 is not larger than num2")
-else:
-    print("num1 is equal to num2")
+# num1 = int(input("num1"))
+# num2 = int(input("num2"))
+# if num1 > num2:
+#     print("num1 is larger than num2")
+# elif num1 < num2:
+#     print("num1 is not larger than num2")
+# else:
+#     print("num1 is equal to num2")
 
 ########################################################################
 # Task 6:
