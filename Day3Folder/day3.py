36ec84c1057bb5cf1e8d016f17961ebf8b0748f6
glee1822 <geelee2219@gmail.com>
2026-06-16 01:31:35 +0000

    Jun 16, 2026, 1:31 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -52,7 +52,7 @@ print("hello from day3")
 num1 = int(input("num1"))
 num2 = int(input("num2"))
 if num1 > num2:
-    print
+    print("num1 is larger than num2")
 
 
 ########################################################################
