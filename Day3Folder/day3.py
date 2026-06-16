2a558094d86da4a3ccc8d7566af405f122944dff
glee1822 <geelee2219@gmail.com>
2026-06-16 01:42:53 +0000

    Jun 16, 2026, 1:42 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -80,8 +80,9 @@ import random
 num1 = random.randint(1,10)
 num2 = random.randint(1,10)
 for i in range(10):
-    answer = input("what is " + num1 + " + " + num2 +" ?")
-    
+    answer = int(input("what is " + num1 + " + " + num2 +" ?"))
+    if answer == num1 + num2:
+        
 
 
 ########################################################################
