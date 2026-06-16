9984c32470bc44b30312599058a6a58ab22373b4
glee1822 <geelee2219@gmail.com>
2026-06-16 04:27:23 +0000

    Jun 16, 2026, 4:27 AM

--- Day2Folder/qns.py
+++ Day2Folder/qns.py
@@ -23,5 +23,7 @@ print("the lowest number is " + str(lownum))
         
 for i in range (len(numbers)):
     totalnum += numbers[i]
-    print("the total of the numbers is " + str(total))
+print("the total of the numbers is " + str(totalnum))
+
+
 
