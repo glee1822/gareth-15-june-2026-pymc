f2e1f6e215b55c012dc6370c52159b742aab0b5f
glee1822 <geelee2219@gmail.com>
2026-06-16 01:50:29 +0000

    Jun 16, 2026, 1:50 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -95,10 +95,11 @@ print("hello from day3")
 # Additional exercises:
 password = input("what is the correct password")
 tries = 3
-for i in range(3):
+for i in range(tries):
     ur_pass = input("wat is ur password")
     if ur_pass == password:
         print("correct")
         break
     else:
+        tries
         print(f"wrong, u have {tries} more tries")
