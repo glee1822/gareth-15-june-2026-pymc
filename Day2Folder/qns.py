d5d025622eb730c75a90d166df7d1ae3d85dac0d
glee1822 <geelee2219@gmail.com>
2026-06-16 04:28:48 +0000

    Jun 16, 2026, 4:28 AM

--- Day2Folder/qns.py
+++ Day2Folder/qns.py
@@ -26,6 +26,6 @@ for i in range (len(numbers)):
     totalnum += numbers[i]
 print("the total of the numbers is " + str(totalnum))
 avgnum = totalnum / len(numbers)
-print(stra(vgnum))
+print("the average of the numbers is " + str(avgnum))
 
 
