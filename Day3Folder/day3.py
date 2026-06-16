e7da001817570ad221d449fc3a69c4a52908e75e
glee1822 <geelee2219@gmail.com>
2026-06-16 01:44:41 +0000

    Jun 16, 2026, 1:44 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -81,7 +81,7 @@ num1 = random.randint(1,10)
 num2 = random.randint(1,10)
 score=0
 for i in range(10):
-    answer = int(input("what is " + str(num1) + " + " + num2 +" ?"))
+    answer = int(input("what is " + str(num1) + " + " + str(num2) +" ?"))
     if answer == num1 + num2:
         print("correct")
         score = score + 1
