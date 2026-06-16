15e0b303c2dbe60da31e9555d78852f64c231b2f
glee1822 <geelee2219@gmail.com>
2026-06-16 04:32:22 +0000

    Jun 16, 2026, 4:32 AM

--- Day2Folder/qns.py
+++ Day2Folder/qns.py
@@ -4,30 +4,34 @@
 import random
 numbers = []
 highnum = 0
-lownum = 100
+lownum = 1001
 totalnum = 0
 avgnum = 0
 seqnum = 0
+arrangednumbers = []
 for i in range(100):
     x = random.randint(1,1000)
     numbers.append(x)
 
-for i in range (len(numbers)):
+for i in range(len(numbers)):
     print(numbers[i])
     if numbers[i] > highnum:
         highnum = numbers[i]
 print("the highest number is " + str(highnum))
 
-for i in range (len(numbers)):
+for i in range(len(numbers)):
     if numbers[i] < lownum:
         lownum = numbers[i]
 print("the lowest number is " + str(lownum))
         
-for i in range (len(numbers)):
+for i in range(len(numbers)):
     totalnum += numbers[i]
 print("the total of the numbers is " + str(totalnum))
 avgnum = totalnum / len(numbers)
 print("the average of the numbers is " + str(avgnum))
 
+for i in range(len(numbers)):
+    if numbers[i] > lownum:
+        
 
 
