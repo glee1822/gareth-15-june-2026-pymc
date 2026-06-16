c074df3d4c887eebaf3b0f139848363b2672db8f
glee1822 <geelee2219@gmail.com>
2026-06-16 01:28:06 +0000

    Jun 16, 2026, 1:28 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -39,7 +39,7 @@ for i in range(total_type_items):
     amount_of_items = 0
     current_item = input("what is the current item you are buying")
     price_current = int(input("what is the price of one of " + current_item))
-    amount_of_items = int(input("how many of " + price_current + "do you want to buy"))
+    amount_of_items = int(input("how many of " + current_item + "do you want to buy"))
     total_price_of_current = price_current * amount_of_items
     print("the total price of" + current_item + "is" + total_price_of_current)
     total_cost_of_all = total_cost_of_all + total_price_of_current
