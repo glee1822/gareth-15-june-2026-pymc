ae11e62619a1a35edd5e13e960026fd6c2e39145
glee1822 <geelee2219@gmail.com>
2026-06-17 03:12:56 +0000

    Jun 17, 2026, 3:12 AM

--- Day3Folder/minecraft_day3.py
+++ Day3Folder/minecraft_day3.py
@@ -1,11 +1,11 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
 
+def wall(length,height):
+    for i in range(height):
+        up(1)
+        for i in range(31):
+           place(DOWN)
+            front(1)
+        turn()
 
-for i in range(3):
-    up(1)
-    for i in range(31):
-        place(DOWN)
-        front(1)
-    turn()
-    
