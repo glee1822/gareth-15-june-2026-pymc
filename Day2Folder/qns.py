25c17f0fb468e68ef194f18dbc4c7f940c6a1acc
glee1822 <geelee2219@gmail.com>
2026-06-17 01:44:44 +0000

    Jun 17, 2026, 1:44 AM

--- Day2Folder/qns.py
+++ Day2Folder/qns.py
@@ -38,7 +38,7 @@ for a in range(100):
     numbers.remove(lownum)
     arrangednumbers.append(lownum)
     lownum = 1000
-print(f"the rearranged list of numbers in order isarrangednumbers")
+print(f"the rearranged list of numbers in order is {arrangednumbers}")
 # for a more general approach you need to consider if the arranged number is odd or even
 
 # .index(element) the element inside is the element in the list
