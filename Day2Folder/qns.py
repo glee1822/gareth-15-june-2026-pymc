48af578178a9620110094b7d42eef80206744b0a
glee1822 <geelee2219@gmail.com>
2026-06-17 01:38:46 +0000

    Jun 17, 2026, 1:38 AM

--- Day2Folder/qns.py
+++ Day2Folder/qns.py
@@ -49,8 +49,9 @@ print(arrangednumbers)
 
 halfway_arrangednumbers = len(arrangednumbers) / 2 # this one will give you float 
 # to prevent having float there are two ways.
-#1. you can use floor()
-#2. you can use /
+#1. you can use math.floor()
+#2. you can use // 
+
 if halfway_arrangednumbers%2  != 0:
     median = arrangednumbers[halfway_arrangednumbers]
     print(median)
