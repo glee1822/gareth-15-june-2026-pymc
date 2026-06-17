bed3c178137a4f2e04f4f7ea6af86094382345ca
glee1822 <geelee2219@gmail.com>
2026-06-17 01:42:40 +0000

    Jun 17, 2026, 1:42 AM

--- Day2Folder/qns.py
+++ Day2Folder/qns.py
@@ -62,7 +62,7 @@ else:
     # after you divide 2
     # you access the list[number//2] and acces the list[number//2 + 1]
     # add them together and get the average
-    median = arrangednumbers[halfway_arrangednumbers + 1] / 2
+    median = (arrangednumbers[halfway_arrangednumbers] + arrangednumbers[halfway_arrangednumbers + 1]) / 2
     print(median)
 
 
