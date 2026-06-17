92ef2f2bd3d229bb94e0dee7f7ed574ede5ecd22
glee1822 <geelee2219@gmail.com>
2026-06-17 03:02:52 +0000

    Jun 17, 2026, 3:02 AM

--- Day2Folder/unique_num_list.py
+++ Day2Folder/unique_num_list.py
@@ -4,8 +4,8 @@ import random
 numbers = []
 
 x = 0
-for i in range(100):
-    numbers.append(random.randint(1,150))
+for i in range(10):
+    numbers.append(random.randint(1,15))
 
 for i in range(len(numbers)):
     x = numbers[i]
