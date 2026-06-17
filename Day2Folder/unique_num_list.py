6d59f7a6baa6fdc525541aad3384bf1cde40e2fb
glee1822 <geelee2219@gmail.com>
2026-06-17 03:02:24 +0000

    Jun 17, 2026, 3:02 AM

--- Day2Folder/unique_num_list.py
+++ Day2Folder/unique_num_list.py
@@ -2,18 +2,18 @@
 
 import random
 numbers = []
-counter = 1
+
 x = 0
 for i in range(100):
     numbers.append(random.randint(1,150))
 
 for i in range(len(numbers)):
-    x = numbers[counter]
+    x = numbers[i]
     if x in numbers == True:
        while x in numbers:
            numbers.remove(x)
            if x not in numbers:
                break
-    counter += 1
+
 print(numbers)
 
